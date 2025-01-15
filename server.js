236e826530cea6083b2cb79b5dbdae4fe9c10cfb
Lucas Alves <lucassalves005@gmail.com>
2025-01-15 20:44:18 -0300

    todo: fix paths

---
+++ server.js
@@ -0,0 +1,41 @@
+//creating a local server for our application
+
+// 1 create directory / 2 create index.js file / 3 initialise npm / 4 install express package /
+// 5 write server application on index.js / 6 start server
+
+//BACKEND FILE
+
+//importing express
+import express from "express";
+const app = express();
+const port = 3000;
+
+// serving static files
+app.use(express.static("public"));
+
+//making server handle GET requests 
+app.get("/", (req, res) => {
+    res.send("Got! Hello world");
+});
+
+//making server handle other endpoints (url.com/#)
+
+// endpoint 1 - contact
+app.get("/contact", (req, res) => {
+    res.send("<h1>Talk to us</h1> <br> 21 98777 5556")
+})
+
+// endpoint 2 - about
+app.get("/about", (req, res) => {
+    res.send("<h1> About us </h1> <br> <p> Lorem ipsum dolor sit amet quasquen pasluis colonistus comutari</p>")
+})
+
+// endpoint 3 - request
+app.get("/request", (req, res) => {
+    res.send("<style> h1{background-color: red; color: white; }</style>  <h1>Requested</h1>")
+})
+
+// shown info on terminal when initialized
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
