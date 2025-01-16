7fec83cf57b9b11d18a54117447e4023898ff172
Lucas Alves <lucassalves005@gmail.com>
2025-01-16 10:37:18 -0300

    frontend files comments fixed

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,8 @@
+//Is the script linked to the index.html file in order to:
+
+// Adds an event listener to the document to execute the callback
+// only after the DOM content has fully loaded and is ready to be interacted with.
 document.addEventListener("DOMContentLoaded", () => {
+    // Logs a message to the console indicating that the frontend script is running.
     console.log("Front-end is running!");
 });
